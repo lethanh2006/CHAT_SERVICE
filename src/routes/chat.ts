7202e176fb1e6f89f3ef432c84be4eb3771f3daf
lethanh2006 <thanhld2006.ptit@gmail.com>
2026-06-25 10:24:23 +0700

    chuyển swagger

--- src/routes/chat.ts
+++ src/routes/chat.ts
@@ -5,86 +5,9 @@ import { upload } from '../middleware/multer.js';
 
 const router = express.Router();
 
-/**
- * @swagger
- * /chat/new:
- *   post:
- *     summary: Tạo cuộc trò chuyện mới
- *     tags: [CHAT]
- *     security:
- *       - bearerAuth: []
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               otherUserId:
- *                 type: string
- *     responses:
- *       200:
- *         description: Hoàn thành
- */
-router.post("/chat/new",isAuth, createNewChat);
-
-/**
- * @swagger
- * /chat/all:
- *   get:
- *     summary: Lấy danh sách tất cả trò chuyện
- *     tags: [CHAT]
- *     responses:
- *       200:
- *         description: Hoàn thành
- */
-router.get("/chat/all",isAuth, getAllChats );
-/**
- * @swagger
- * /message:
- *   post:
- *     summary: Gửi tin nhắn mới (chứa text hoặc file ảnh)
- *     tags: [CHAT]
- *     security:
- *       - bearerAuth: []
- *     requestBody:
- *       content:
- *         multipart/form-data:
- *           schema:
- *             type: object
- *             properties:
- *               chatId:
- *                 type: string
- *               text:
- *                 type: string
- *               image:
- *                 type: string
- *                 format: binary
- *     responses:
- *       200:
- *         description: Gửi thành công
- */
+router.post("/chat/new", isAuth, createNewChat);
+router.get("/chat/all", isAuth, getAllChats);
 router.post("/message", isAuth, upload.single('image'), sendMessage);
-
-/**
- * @swagger
- * /message/{chatId}:
- *   get:
- *     summary: Lấy danh sách tin nhắn theo ID cuộc trò chuyện
- *     tags: [CHAT]
- *     security:
- *       - bearerAuth: []
- *     parameters:
- *       - in: path
- *         name: chatId
- *         required: true
- *         schema:
- *           type: string
- *         description: ID của cuộc trò chuyện
- *     responses:
- *       200:
- *         description: Success
- */
-router.get("/message/:chatId", isAuth , getMessagesByChat);
+router.get("/message/:chatId", isAuth, getMessagesByChat);
 
 export default router;
